e08359d968a8797d6cf1cd42c82531fc12f6366e
vincet0t15 <vincet0t15@gmail.com>
2026-03-24 07:57:28 +0800

    asdf

--- resources/js/layouts/app-layout.tsx
+++ resources/js/layouts/app-layout.tsx
@@ -1,6 +1,6 @@
+import { TooltipProvider } from '@/components/ui/tooltip';
 import AppLayoutTemplate from '@/layouts/app/app-sidebar-layout';
 import { type BreadcrumbItem } from '@/types';
-
 interface AppLayoutProps {
     children: React.ReactNode;
     breadcrumbs?: BreadcrumbItem[];
@@ -8,6 +8,7 @@ interface AppLayoutProps {
 
 export default ({ children, breadcrumbs, ...props }: AppLayoutProps) => (
     <AppLayoutTemplate breadcrumbs={breadcrumbs} {...props}>
-        {children}
+        <TooltipProvider>{children}</TooltipProvider>
+        {/* {children} */}
     </AppLayoutTemplate>
 );
