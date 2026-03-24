592724e6d79ec9f0863a443c408e66b2676ce729
vincet0t15 <vincet0t15@gmail.com>
2026-03-24 16:00:04 +0800

    asdf

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\Storage;
 
 class Employee extends Model
 {
@@ -16,7 +17,6 @@ class Employee extends Model
         'office_id',
         'image',
     ];
-
     public function fingerprints()
     {
         return $this->hasMany(Fingeprint::class);
@@ -25,5 +25,10 @@ class Employee extends Model
     public function office()
     {
         return $this->belongsTo(Office::class);
+    }
+
+    public function getImagePathAttribute($value)
+    {
+        return $value ? Storage::url($value) : null;
     }
 }
